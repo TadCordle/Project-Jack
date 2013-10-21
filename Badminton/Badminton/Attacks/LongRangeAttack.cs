e2942616424f3ef78566e29c8bf7059bb90f5221
Tad Cordle <jellyfish646@aim.com>
2013-10-21 19:51:44 -0400

    Fixed weird physics stuff with long range attack

--- Badminton/Badminton/Attacks/LongRangeAttack.cs
+++ Badminton/Badminton/Attacks/LongRangeAttack.cs
@@ -19,21 +19,12 @@ namespace Badminton.Attacks
 		public LongRangeAttack(World w, Vector2 position, Vector2 power, float damage, Category collisionCat)
 			: base(w, position, power, damage, collisionCat)
 		{
-			body = BodyFactory.CreateRectangle(w, 100f * MainGame.PIXEL_TO_METER, 16f * MainGame.PIXEL_TO_METER, 5000f);
-			body.BodyType = BodyType.Dynamic;
-			body.Position = position;
-			body.LinearVelocity = power;
-			this.collisionCat = collisionCat;
-			body.CollisionCategories = collisionCat;
-			body.UserData = this;
-			this.damage = damage;
-
 			body.OnCollision += new OnCollisionEventHandler(HitWall);
 		}
 
 		private bool HitWall(Fixture f1, Fixture f2, Contact c)
 		{
-			if (f2.Body.UserData is Wall || f2.Body.UserData is Attack)
+			if (f2.Body.UserData is Wall)
 				body.UserData = null;
 			return true;
 		}
