dcc159b1e28f1b44ae69d32654f5d0423132dd34
Michael <davism4@umbc.edu>
2014-04-22 01:04:12 -0400

    bot ai, more human-like, projectiles

--- Badminton/Badminton/Stick Figures/BotPlayer.cs
+++ Badminton/Badminton/Stick Figures/BotPlayer.cs
@@ -19,22 +19,24 @@ namespace Badminton.Stick_Figures
     {
         private PlayerIndex player;
         private StickFigure target;
-        private float attention_radius, attention_countdown, attention_countdown_max, attention_countdown_delta;
-       // private bool hasTarget;
-        private float melee_radius;
-
+        private float newtarget_countdown = 0.1f, delta_time=0.006f, move_countdown, melee_countdown, shoot_countdown;
+        private bool should_walk_left = false, should_walk_right = false, should_jump = false;
+        private float angle;
+        
+        private Random random;
         public StickFigure[] ListStickFigures;
-
+            
         public BotPlayer(World world, Vector2 position, Category collisionCat, float scale, float limbStrength, float limbDefense, bool evilSkin, Color color, PlayerIndex player, StickFigure[] dudes)
             : base(world, position, collisionCat, scale, limbStrength, limbDefense, evilSkin, color)
         {
             this.player = player;
             this.target = null;
-            attention_radius = 100;
+            //attention_radius = 100;
             this.ListStickFigures = dudes;
-            attention_countdown_max = 5f;
-            attention_countdown = attention_countdown_max;
-            attention_countdown_delta = 0.0075f;
+            random = new Random();
+            move_countdown = 0.05f + 0.15f * (float)random.NextDouble(); // initial delay 0.05-0.25s
+            melee_countdown = 0.1f + 0.5f * (float)random.NextDouble(); // initial delay 0.1-0.4s
+            shoot_countdown = 0.2f + 0.5f * (float)random.NextDouble(); // initial delay 0.2-0.7s
         }
 
 		public override StickFigure Respawn()
@@ -42,118 +44,165 @@ namespace Badminton.Stick_Figures
             return new BotPlayer(world, startPosition, collisionCat, scale, limbStrength, limbDefense, EvilSkin, color, player, this.ListStickFigures);
 		}
 
+        private void ResetTarget()
+        {
+            float closestDist = 999f, dist;
+            foreach (StickFigure s in ListStickFigures)
+            {
+                if (s != this && !s.IsDead && (s.CollisionCategory != this.collisionCat) && !IsRaycast(s.Position))
+                {
+                    if ((this.target != null) && (s is BotPlayer) && (float)random.NextDouble() < 0.25f)
+                    {
+                        continue; // sometimes prefer humans
+                    }
+                    dist = (s.Position - this.Position).Length();
+                    if (((float)random.NextDouble() < 0.6f) && (dist < closestDist))
+                    {
+                        closestDist = dist; // sometimes not pick the closest guy
+                        this.target = s;
+                    }
+                }
+            }    
+        }
+
+        private bool IsRaycast(Vector2 v)
+        {
+            bool b=false;
+            world.RayCast((f, p, n, fr) =>
+            {
+                if (f != null && f.Body.UserData is Wall)
+                {
+                    b = true;
+                    return 1; // raycast is dumb
+                }
+                else
+                {
+                    b = false;
+                    return 0;
+                }
+            }, Position, v);
+            return b;
+        }
+        
         public override void Update()
         {
             if (!IsDead)
             {
                 bool stand = true;
-                // get target
 
-                if (this.target == null)// || (this.target is BotPlayer))
+                // Before determining movement or anything, determine whether to blow up
+                if (melee_countdown <= 0 || shoot_countdown <= 0)
                 {
-                    float closestDist = 999f, dist;
-                    Random r = new Random();
-                    // May pick nearest stick figure, or nearest player-controlled stick figure.
-                    // Makes the game more challenging.
-                    if (r.Next(10) > 4)
+                    if (ScalarHealth < 0.1f)
                     {
+                        bool b = false;
                         foreach (StickFigure s in ListStickFigures)
                         {
-                            if (s != this && !s.IsDead && (s.CollisionCategory != this.collisionCat))
-                            {
-                                dist = (s.Position - this.Position).Length() + (float)r.NextDouble() * 1.5f;
-                                if (dist < closestDist)
-                                {
-                                    closestDist = dist;
-                                    this.target = s;
-                                }
-                            }
+                            if (s != this && !s.IsDead && (s.CollisionCategory != this.collisionCat) && (s.Position - this.Position).Length() < 1.5f)
+                                b = true;
                         }
-                    }
-                    else
-                    {
-                        foreach (StickFigure s in ListStickFigures)
+                        if (b)
                         {
-                            if (!(s is BotPlayer) && !s.IsDead && (s.CollisionCategory != this.collisionCat))
-                            {
-                                dist = (s.Position - this.Position).Length() + (float)r.NextDouble() * 1.5f;
-                                if (dist < closestDist)
-                                {
-                                    closestDist = dist;
-                                    this.target = s;
-                                }
-                                // check collisioncat
-                                /*  if ((s.Position - this.Position).Length() <= attention_radius)
-                                  {
-                                      this.hasTarget = true;
-                                      this.target = s;
-                                      break;
-                                  }*/
-                            }
+                            Explode();
+                            return;
                         }
                     }
+                }
 
-                }
-                else if (this.target.IsDead)
+                // first, check the target
+                if ((this.target == null) || this.target.IsDead || (newtarget_countdown<=0))
                 {
-                    this.target = null;
+                    ResetTarget();
+                    newtarget_countdown = 4f;
                 }
-                else
+
+                // check motion
+                if (move_countdown>0) // continue the current motion
                 {
-                    if (target.Position.X > this.Position.X)
-                    {
-                        WalkRight();
-                        stand = false;
-                    }
-                    else if (target.Position.X <= this.Position.X)
+                    if (should_walk_left)
                     {
                         WalkLeft();
                         stand = false;
                     }
-                    if (target.Position.Y < this.Position.Y - 4)
+                    else if (should_walk_right)
+                    {
+                        WalkRight();
+                        stand = false;
+                    }
+                    if (should_jump)
                     {
                         Jump();
                         stand = false;
-                    }
-
-                    if ((target.Position - this.Position).Length() < 1) // ADJUST AS NEEDED
-                    {
-                        if (target.Position.X > this.Position.X)
-                        {
-                            Punch(0);
-                        }
-                        else
-                        {
-                            Punch(MathHelper.Pi);
-                        }
-                    }
-                    else if (((target.Position - this.Position).Length() < 2.5) && // ADJUST AS NEEDED
-                        (target.Position.Y > this.Position.Y))
-                    {
-                        if (target.Position.X > this.Position.X)
-                        {
-                            Kick(0);
-                        }
-                        else
-                        {
-                            Kick(MathHelper.Pi);
-                        }
+                        should_jump = false; // only jump once
                     }
                 }
-
-                if (stand)
-                    Stand();
-
-                // After doing stuff, determine whether should check for better target
-                if (attention_countdown <= 0)
+                else if (target != null)
                 {
-                    this.target = null;
-                    attention_countdown = attention_countdown_max;
+                    // choose new direction every 0.1-0.5 sec
+                    move_countdown = 0.1f + 0.4f * (float)random.NextDouble();
+                    should_walk_left = false;
+                    should_walk_right = false;
+                    should_jump = false;
+                    if (target.Position.X - this.Position.X > 0.1f)
+                    {
+                        should_walk_right = true;
+                    }
+                    else if (target.Position.X - this.Position.X < -0.1f)
+                    {
+                        should_walk_left = true;
+                    }
+                    if ((target.Position.Y - this.Position.Y < -4f))
+                    {
+                        should_jump = true;
+                    }
                 }
-                else
+                
+                if (target != null) // determine attack
                 {
-                    attention_countdown -= attention_countdown_delta; // how to normalize this to actual game time?
+                    if (melee_countdown<= 0 && (target.Position - this.Position).Length() < 3f)
+                    {
+                        if (Math.Abs(target.Position.Y - this.Position.Y) < 0.1f) // approximate left/right
+                        {
+                            if (target.Position.X > this.Position.X)
+                                Punch(0f); //  right
+                            else
+                                Punch(MathHelper.Pi); // left
+                        }
+                        else if (Math.Abs(target.Position.X - this.Position.X) < 0.1f) // approximate up/down
+                        {
+                            if (target.Position.Y < this.Position.Y) // above
+                                Punch(MathHelper.PiOver2);
+                            else // below
+                                Kick(-MathHelper.PiOver2);
+                        }
+                        else // need to calculate angle
+                        {
+                            angle = (float)Math.Atan((-target.Position.Y + this.Position.Y) / (target.Position.X - this.Position.X));
+                            if (target.Position.Y < this.Position.Y)
+                                Punch(angle);
+                            else
+                                Kick(angle);
+                        }
+                        melee_countdown = 0.1f + 0.1f*(float)random.NextDouble(); // delay 0.1-0.2s
+                    }
+                    else if (shoot_countdown <= 0)// && (target.Position - this.Position).Length() > 5f)
+                    { // too far -> shoot projectile
+                        angle = (float)Math.Atan((-target.Position.Y + this.Position.Y) / (target.Position.X - this.Position.X));
+                        Aim(angle);
+                        LongRangeAttack();
+                        shoot_countdown = 0.5f + (float)random.NextDouble(); // delay 0.5-1.5 sec
+                    }
                 }
+                if (stand) Stand();
+                // Decrement countdowns
+                if (newtarget_countdown > 0)
+                    newtarget_countdown -= delta_time;
+                if (move_countdown > 0)
+                    move_countdown -= delta_time;
+                if (melee_countdown > 0)
+                    melee_countdown -= delta_time;
+                if (shoot_countdown > 0)
+                    shoot_countdown -= delta_time;
             }
             base.Update();
         }
